a994f7913b1e467faa77e196b8269ef5f04bf9d8
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-12-11 14:03:50 +0100

    windowwidth

--- data analitics/src/App.jsx
+++ data analitics/src/App.jsx
@@ -1,21 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { HiArrowNarrowRight, HiMenu } from "react-icons/hi";
 import Menu from "./Menu";
 function App() {
   const [menuOn, setMenuOn] = useState(false);
+  // const [windowWidth, setWindowWidth] = window.innerWidth;
   const Toggle = () => {
     setMenuOn((prevMenu) => !prevMenu);
   };
+
+  const handleResize = () => {
+    window.innerWidth > 600 ? setMenuOn(true) : setMenuOn(false);
+  };
+
   const marginTopStyle = {
     marginTop: menuOn ? "2.5rem" : "15rem",
   };
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
       <div className="container">
         <header className="header">
-          <button className="menu--btn" onClick={Toggle}>
-            <HiMenu className="header--icon" />
-          </button>
+          {window.innerWidth <= 600 ? (
+            <button className="menu--btn" onClick={Toggle} disabled={false}>
+              <HiMenu className="header--icon" />
+            </button>
+          ) : null}
 
           <h1 className="logo">Logo</h1>
         </header>
