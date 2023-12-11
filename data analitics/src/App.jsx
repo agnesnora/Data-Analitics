e60a6f08f97cfbb1971cdd38c9a8c11cf569f851
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-12-11 11:45:05 +0100

    mobile first styling

--- data analitics/src/App.jsx
+++ data analitics/src/App.jsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
-
+import { HiArrowNarrowRight, HiMenu } from "react-icons/hi";
 function App() {
   return (
     <>
-      <div>
-        <p>Menu</p> <h1 className="logo">Logo</h1>
-      </div>
-      <div>
-        <main className="main--container">
+      <div className="container">
+        <header className="header">
+          <HiMenu className="header--icon" />
+          <h1 className="logo">Logo</h1>
+        </header>
+        <div>
           <h1 className="main--title">Data Analytics</h1>
-          <h2>Making sense of your traffic</h2>
-          <button className="go--btn">Go to my stats</button>
-        </main>
+          <h2>
+            Making sense of <br />
+            your traffic
+          </h2>
+
+          <button className="go--btn">
+            {" "}
+            <HiArrowNarrowRight className="icon" /> Go to my stats
+          </button>
+        </div>
       </div>
     </>
   );
